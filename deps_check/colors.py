0303698b1631c6a4b45c563ff153dc79580e1c2b
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:57:14 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- deps_check/colors.py
+++ deps_check/colors.py
@@ -1,8 +1,8 @@
 # Deps test
 # Run a set of tests or checks to see if commands run successfully or files exist
-# GitHub: https://www.github.com/lewisevans2007/deps_check
+# GitHub: https://www.github.com/0x4248/deps_check
 # Licence: GNU General Public License v3.0
-# By: Lewis Evans
+# By: 0x4248
 
 RED = "\33[31m"
 GREEN = "\33[32m"
